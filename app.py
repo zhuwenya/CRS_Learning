2c29409c8a4a34c9e25a16928975bf433992c26b
zhuwenya <wzhuaf@connect.ust.hk>
2016-10-17 15:11:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,38 +24,15 @@ class wechat:
         return echostr if checkstr == signature else 'Fail'
 
     def POST(self):
-       
-        print(Recommend_Agent.num)
         if Recommend_Agent.num==0:
             data=web.data()
             Recommend_Agent.num=1
             web.header('Content-Type', 'text/xml')
-            return self.process_zero_message()          
-        elif(Recommend_Agent.num==1):
-            data = web.data()
-            Recommend_Agent.num=2
-            web.header('Content-Type', 'text/xml')
-            return self.process_first_message(data)
-        elif(Recommend_Agent.num==2):
-            data = web.data()
-            Recommend_Agent.num=3
-            web.header('Content-Type', 'text/xml')
-            return self.process_second_message(data)
+            return self.process_first_message()          
         else:
             data = web.data()
             web.header('Content-Type', 'text/xml')
             return self.process_message(data)
-
-    def process_zero_message(self):
-        def get_tag(node, name):
-            return root.getElementsByTagName(name)[0].childNodes[0].data
-        root = minidom.parseString(message)
-        toUser = get_tag(root, 'ToUserName')
-        fromUser = get_tag(root, 'FromUserName')
-        msgType = 'text'
-        response = self.Recommend_Agent.get_zero_response()
-        createTime = int(time.time())
-        return wechat.render.response(fromUser, toUser, createTime, response)
 
     def process_first_message(self,message):
         def get_tag(node, name):
@@ -65,19 +42,10 @@ class wechat:
         fromUser = get_tag(root, 'FromUserName')
         msgType ='text'
         response = Recommend_Agent.get_first_response()
+        response="欢迎来到对话推荐系统,CRS向您推荐咖啡等其他饮品"+"友情提示：如果您想离开此系统，请输入q"+'\n'+'开始体验'+'\n'+response
         createTime = int(time.time())
         return wechat.render.response(fromUser, toUser, createTime, response)
 
-    def process_second_message(self):
-        def get_tag(node, name):
-            return root.getElementsByTagName(name)[0].childNodes[0].data
-        root = minidom.parseString(message)
-        toUser = get_tag(root, 'ToUserName')
-        fromUser = get_tag(root, 'FromUserName')
-        msgType = 'text'
-        response = self.Recommend_Agent.get_zero_response()
-        createTime = int(time.time())
-        return wechat.render.response(fromUser, toUser, createTime, response)
     def process_message(self, message):
         def get_tag(node, name):
             return root.getElementsByTagName(name)[0].childNodes[0].data
