98be39ca5a164c00bd4695e7d99fca2ba729bd91
zhuwenya <zhuwenya1991@gmail.com>
2016-10-17 22:59:17 +0800

    --all

--- app.py
+++ app.py
@@ -4,11 +4,18 @@ import xml.dom.minidom as minidom
 import backend
 from backend import *
 import time
+import manage_user
+from manage_user import *
+import json
 
-Recommend_Agent=recommend_agent()
+f=open('/home/ubuntu/Collect_Data/CRS_Learning/data/wechat_data.txt','w')
+User_Management=Manage_User()
+def get_tag(node, name):
+    return root.getElementsByTagName(name)[0].childNodes[0].data
+
 class wechat:
     render = web.template.render('templates/')
-    
+
     def GET(self):
         # For developer verification
         get_params = web.input()
@@ -24,43 +31,31 @@ class wechat:
         return echostr if checkstr == signature else 'Fail'
 
     def POST(self):
-        if Recommend_Agent.num==0:
-            data=web.data()
-            Recommend_Agent.num=1
-            web.header('Content-Type', 'text/xml')
-            return self.process_first_message()          
-        else:
-            data = web.data()
-            web.header('Content-Type', 'text/xml')
-            return self.process_message(data)
-
-    def process_first_message(self,message):
-        def get_tag(node, name):
-            return root.getElementsByTagName(name)[0].childNodes[0].data
-        root = minidom.parseString(message)
-        toUser = get_tag(root, 'ToUserName')
-        fromUser = get_tag(root, 'FromUserName')
-        msgType ='text'
-        response = Recommend_Agent.get_first_response()
-        response="欢迎来到对话推荐系统,CRS向您推荐咖啡等其他饮品"+"友情提示：如果您想离开此系统，请输入q"+'\n'+'开始体验'+'\n'+response
-        createTime = int(time.time())
-        return wechat.render.response(fromUser, toUser, createTime, response)
-
-    def process_message(self, message):
-        def get_tag(node, name):
-            return root.getElementsByTagName(name)[0].childNodes[0].data
-        root = minidom.parseString(message)
+        data=web.data()
+        Recommend_Agent.num=1
+        web.header('Content-Type', 'text/xml')
+        root = minidom.parseString(data)
         toUser = get_tag(root, 'ToUserName')
         fromUser = get_tag(root, 'FromUserName')
         msgType = get_tag(root, 'MsgType')
-
-        if msgType == u'text':
-            content = get_tag(root, 'Content')
-            #response="nihao"
-            response =Recommend_Agent.get_response(content)
-        else:
-            response = '{} is not supported yet.'.format(msgType.capitalize())
+        content = get_tag(root, 'Content')
         createTime = int(time.time())
+        Recommend_Agent=User_Management.obtain_user_instance(fromUser,createTime)
+        if Recommend_Agent.num==0:
+            response = Recommend_Agent.get_first_response()
+            response="欢迎来到对话推荐系统,CRS向您推荐咖啡等其他饮品"+"友情提示：如果您想离开此系统，请输入q"+'\n'+'开始体验'+'\n'+response
+        else:
+            if msgType == u'text':
+                content = get_tag(root, 'Content')
+                #response="nihao"
+                response =Recommend_Agent.get_response(content)
+            else:
+                response = '{} is not supported yet.'.format(msgType.capitalize())
+        User_Management.clean_user()
+        data={fromUser:content,toUser:response:'time',createTime}
+        json_str = json.dumps(data)
+        f.write(json_str)
+        f.write('\n')
         return wechat.render.response(fromUser, toUser, createTime, response)
 
 
